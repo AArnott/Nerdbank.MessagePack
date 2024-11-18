3a77ed556c6f915feb7739aa34a4567f3707ee62
Andrew Arnott <andrewarnott@live.com>
2024-11-17 18:54:35 -0700

    Improve xml doc on Extension(Header) structs

--- src/Nerdbank.MessagePack/ExtensionHeader.cs
+++ src/Nerdbank.MessagePack/ExtensionHeader.cs
@@ -6,6 +6,6 @@ namespace Nerdbank.MessagePack;
 /// <summary>
 /// Describes a msgpack extension. This precedes the extension payload itself in the msgpack encoded format.
 /// </summary>
-/// <param name="TypeCode">A value that uniquely identifies the extension type. Negative values are reserved for official msgpack extensions. See <see cref="ReservedMessagePackExtensionTypeCode"/> for values already assigned from the reserved range.</param>
+/// <param name="TypeCode">A value that uniquely identifies the extension type. Negative values are reserved for official msgpack extensions. See <see cref="ReservedMessagePackExtensionTypeCode"/> and <see cref="LibraryReservedMessagePackExtensionTypeCode"/> for values that are already assigned.</param>
 /// <param name="Length">The length of the extension's data payload.</param>
 public record struct ExtensionHeader(sbyte TypeCode, uint Length);
