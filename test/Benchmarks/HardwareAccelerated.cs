8ccd12e1f3b2b1d3b84b1d2631a293a4ef98c3b1
Andrew Arnott <andrewarnott@live.com>
2024-11-23 12:50:35 -0700

    Simplify benchmarks

--- test/Benchmarks/HardwareAccelerated.cs
+++ test/Benchmarks/HardwareAccelerated.cs
@@ -3,7 +3,6 @@
 
 [MemoryDiagnoser]
 [GroupBenchmarksBy(BenchmarkDotNet.Configs.BenchmarkLogicalGroupRule.ByCategory)]
-[CategoriesColumn]
 public partial class HardwareAccelerated
 {
 	private const int Length = 10_000;
@@ -22,33 +21,23 @@ public partial class HardwareAccelerated
 	private static readonly double[] DoubleValues = GetRandomDoubles(Length);
 	private readonly Sequence buffer = new();
 
+	[ParamsAllValues]
+	public bool Accelerated { get; set; }
+
+	public MessagePackSerializer Serializer => this.Accelerated ? AcceleratedSerializer : UnacceleratedSerializer;
+
 	[Benchmark]
 	[BenchmarkCategory("bool", "deserialize")]
 	public void Bool_Deserialize()
 	{
-		AcceleratedSerializer.Deserialize<bool[], Witness>(BoolValuesMsgPack);
-	}
-
-	[Benchmark(Baseline = true)]
-	[BenchmarkCategory("bool", "deserialize")]
-	public void Bool_Deserialize_Unoptimized()
-	{
-		UnacceleratedSerializer.Deserialize<bool[], Witness>(BoolValuesMsgPack);
+		this.Serializer.Deserialize<bool[], Witness>(BoolValuesMsgPack);
 	}
 
 	[Benchmark]
 	[BenchmarkCategory("bool", "serialize")]
 	public void Bool_Serialize()
 	{
-		AcceleratedSerializer.Serialize<bool[], Witness>(this.buffer, BoolValues);
-		this.buffer.Reset();
-	}
-
-	[Benchmark(Baseline = true)]
-	[BenchmarkCategory("bool", "serialize")]
-	public void Bool_Serialize_Unoptimized()
-	{
-		UnacceleratedSerializer.Serialize<bool[], Witness>(this.buffer, BoolValues);
+		this.Serializer.Serialize<bool[], Witness>(this.buffer, BoolValues);
 		this.buffer.Reset();
 	}
 
@@ -56,15 +45,7 @@ public partial class HardwareAccelerated
 	[BenchmarkCategory("int8", "serialize")]
 	public void Int8_Serialize()
 	{
-		AcceleratedSerializer.Serialize<sbyte[], Witness>(this.buffer, Int8Values);
-		this.buffer.Reset();
-	}
-
-	[Benchmark(Baseline = true)]
-	[BenchmarkCategory("int8", "serialize")]
-	public void Int8_Serialize_Unoptimized()
-	{
-		UnacceleratedSerializer.Serialize<sbyte[], Witness>(this.buffer, Int8Values);
+		this.Serializer.Serialize<sbyte[], Witness>(this.buffer, Int8Values);
 		this.buffer.Reset();
 	}
 
@@ -72,15 +53,7 @@ public partial class HardwareAccelerated
 	[BenchmarkCategory("int16", "serialize")]
 	public void Int16_Serialize()
 	{
-		AcceleratedSerializer.Serialize<short[], Witness>(this.buffer, Int16Values);
-		this.buffer.Reset();
-	}
-
-	[Benchmark(Baseline = true)]
-	[BenchmarkCategory("int16", "serialize")]
-	public void Int16_Serialize_Unoptimized()
-	{
-		UnacceleratedSerializer.Serialize<short[], Witness>(this.buffer, Int16Values);
+		this.Serializer.Serialize<short[], Witness>(this.buffer, Int16Values);
 		this.buffer.Reset();
 	}
 
@@ -88,15 +61,7 @@ public partial class HardwareAccelerated
 	[BenchmarkCategory("int32", "serialize")]
 	public void Int32_Serialize()
 	{
-		AcceleratedSerializer.Serialize<int[], Witness>(this.buffer, Int32Values);
-		this.buffer.Reset();
-	}
-
-	[Benchmark(Baseline = true)]
-	[BenchmarkCategory("int32", "serialize")]
-	public void Int32_Serialize_Unoptimized()
-	{
-		UnacceleratedSerializer.Serialize<int[], Witness>(this.buffer, Int32Values);
+		this.Serializer.Serialize<int[], Witness>(this.buffer, Int32Values);
 		this.buffer.Reset();
 	}
 
@@ -104,15 +69,7 @@ public partial class HardwareAccelerated
 	[BenchmarkCategory("int64", "serialize")]
 	public void Int64_Serialize()
 	{
-		AcceleratedSerializer.Serialize<long[], Witness>(this.buffer, Int64Values);
-		this.buffer.Reset();
-	}
-
-	[Benchmark(Baseline = true)]
-	[BenchmarkCategory("int64", "serialize")]
-	public void Int64_Serialize_Unoptimized()
-	{
-		UnacceleratedSerializer.Serialize<long[], Witness>(this.buffer, Int64Values);
+		this.Serializer.Serialize<long[], Witness>(this.buffer, Int64Values);
 		this.buffer.Reset();
 	}
 
@@ -120,15 +77,7 @@ public partial class HardwareAccelerated
 	[BenchmarkCategory("uint16", "serialize")]
 	public void UInt16_Serialize()
 	{
-		AcceleratedSerializer.Serialize<ushort[], Witness>(this.buffer, UInt16Values);
-		this.buffer.Reset();
-	}
-
-	[Benchmark(Baseline = true)]
-	[BenchmarkCategory("uint16", "serialize")]
-	public void UInt16_Serialize_Unoptimized()
-	{
-		UnacceleratedSerializer.Serialize<ushort[], Witness>(this.buffer, UInt16Values);
+		this.Serializer.Serialize<ushort[], Witness>(this.buffer, UInt16Values);
 		this.buffer.Reset();
 	}
 
@@ -136,15 +85,7 @@ public partial class HardwareAccelerated
 	[BenchmarkCategory("uint32", "serialize")]
 	public void UInt32_Serialize()
 	{
-		AcceleratedSerializer.Serialize<uint[], Witness>(this.buffer, UInt32Values);
-		this.buffer.Reset();
-	}
-
-	[Benchmark(Baseline = true)]
-	[BenchmarkCategory("uint32", "serialize")]
-	public void UInt32_Serialize_Unoptimized()
-	{
-		UnacceleratedSerializer.Serialize<uint[], Witness>(this.buffer, UInt32Values);
+		this.Serializer.Serialize<uint[], Witness>(this.buffer, UInt32Values);
 		this.buffer.Reset();
 	}
 
@@ -152,15 +93,7 @@ public partial class HardwareAccelerated
 	[BenchmarkCategory("uint64", "serialize")]
 	public void UInt64_Serialize()
 	{
-		AcceleratedSerializer.Serialize<ulong[], Witness>(this.buffer, UInt64Values);
-		this.buffer.Reset();
-	}
-
-	[Benchmark(Baseline = true)]
-	[BenchmarkCategory("uint64", "serialize")]
-	public void UInt64_Serialize_Unoptimized()
-	{
-		UnacceleratedSerializer.Serialize<ulong[], Witness>(this.buffer, UInt64Values);
+		this.Serializer.Serialize<ulong[], Witness>(this.buffer, UInt64Values);
 		this.buffer.Reset();
 	}
 
@@ -168,15 +101,7 @@ public partial class HardwareAccelerated
 	[BenchmarkCategory("float", "serialize")]
 	public void Single_Serialize()
 	{
-		AcceleratedSerializer.Serialize<float[], Witness>(this.buffer, SingleValues);
-		this.buffer.Reset();
-	}
-
-	[Benchmark(Baseline = true)]
-	[BenchmarkCategory("float", "serialize")]
-	public void Single_Serialize_Unoptimized()
-	{
-		UnacceleratedSerializer.Serialize<float[], Witness>(this.buffer, SingleValues);
+		this.Serializer.Serialize<float[], Witness>(this.buffer, SingleValues);
 		this.buffer.Reset();
 	}
 
@@ -184,15 +109,7 @@ public partial class HardwareAccelerated
 	[BenchmarkCategory("double", "serialize")]
 	public void Double_Serialize()
 	{
-		AcceleratedSerializer.Serialize<double[], Witness>(this.buffer, DoubleValues);
-		this.buffer.Reset();
-	}
-
-	[Benchmark(Baseline = true)]
-	[BenchmarkCategory("double", "serialize")]
-	public void Double_Serialize_Unoptimized()
-	{
-		UnacceleratedSerializer.Serialize<double[], Witness>(this.buffer, DoubleValues);
+		this.Serializer.Serialize<double[], Witness>(this.buffer, DoubleValues);
 		this.buffer.Reset();
 	}
 
