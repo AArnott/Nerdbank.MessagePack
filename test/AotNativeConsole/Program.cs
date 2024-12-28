41d74482d49a7de22f66c6bf99431b00e6aa6d92
Andrew Arnott <andrewarnott@live.com>
2024-12-28 07:41:18 -0700

    Modify AOT console to exercise generics

--- test/AotNativeConsole/Program.cs
+++ test/AotNativeConsole/Program.cs
@@ -1,22 +1,65 @@
 ﻿// Copyright (c) Andrew Arnott. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Diagnostics.CodeAnalysis;
 using Nerdbank.MessagePack;
 using PolyType;
 
-Tree tree = new()
-{
-	Fruits = [new Fruit(3), new Fruit(5)],
-};
+GenericData<int> data = new() { Value = 42 };
 
 MessagePackSerializer serializer = new();
-Tree deserializedTree = serializer.Deserialize<Tree>(serializer.Serialize(tree))!;
-Console.WriteLine($"Fruit count: {deserializedTree.Fruits.Count}");
+GenericData<int> deserializedTree = serializer.Deserialize<GenericData<int>, Witness>(serializer.Serialize<GenericData<int>, Witness>(data))!;
+Console.WriteLine($"Value: {deserializedTree.Value}");
 
-[GenerateShape]
-partial class Tree
+#pragma warning disable NBMsgPack020
+[MessagePackConverter(typeof(GenericDataConverter<>))]
+public partial record GenericData<T>
 {
-	public List<Fruit> Fruits { get; set; } = [];
+	internal T? Value { get; set; }
 }
 
-partial class Fruit(int Seeds);
+partial class GenericDataConverter<T> : MessagePackConverter<GenericData<T>>
+{
+	public GenericDataConverter()
+	{
+	}
+
+	public override GenericData<T>? Read(ref MessagePackReader reader, SerializationContext context)
+	{
+		if (reader.TryReadNil())
+		{
+			return null;
+		}
+
+		int count = reader.ReadArrayHeader();
+		if (count != 1)
+		{
+			throw new MessagePackSerializationException("Expected array of length 1.");
+		}
+
+		return new GenericData<T>
+		{
+			Value = GetTConverter(ref context).Read(ref reader, context),
+		};
+	}
+
+	public override void Write(ref MessagePackWriter writer, in GenericData<T>? value, SerializationContext context)
+	{
+		if (value is null)
+		{
+			writer.WriteNil();
+			return;
+		}
+
+		writer.WriteArrayHeader(1);
+		GetTConverter(ref context).Write(ref writer, value.Value, context);
+	}
+
+	private static MessagePackConverter<T> GetTConverter(ref SerializationContext context)
+		=> (MessagePackConverter<T>)context.GetConverter(typeof(T), null);
+}
+
+[GenerateShape<GenericData<int>>]
+[GenerateShape<GenericDataConverter<int>>]
+[GenerateShape<int>] // PolyType: why is this necessary? Shouldn't it be inferred?
+partial class Witness;
