ae769c9c9efc2dd08c1e1692780a9065852d828d
Andrew Arnott <andrew.arnott@microsoft.com>
2025-02-27 16:22:31 -0700

    Generalize more tests

--- test/ShapeShift.Tests/AsyncReaderTests.cs
+++ test/ShapeShift.Tests/AsyncReaderTests.cs
@@ -3,15 +3,13 @@
 
 #pragma warning disable NBMsgPackAsync
 
-using ShapeShift.MessagePack;
-
-public class MessagePackAsyncReaderTests
+public abstract class AsyncReaderTests(SerializerBase serializer) : SerializerTestBase(serializer)
 {
 	[Fact]
 	public async Task BufferNextStructureAsync_IncompleteBuffer()
 	{
 		Sequence<byte> seq = new();
-		Writer writer = new(seq, MessagePackFormatter.Default);
+		Writer writer = new(seq, this.Serializer.Formatter);
 		writer.WriteStartVector(3);
 		writer.Write(1);
 		writer.Write(2);
@@ -22,7 +20,11 @@ public class MessagePackAsyncReaderTests
 		FragmentedPipeReader pipeReader = new(ros, ros.GetPosition(1));
 
 		SerializationContext context = new();
-		using AsyncReader reader = new(pipeReader, MessagePackDeformatter.Default) { CancellationToken = default };
+		using AsyncReader reader = new(pipeReader, this.Serializer.Deformatter) { CancellationToken = default };
 		await reader.BufferNextStructureAsync(context);
 	}
+
+	public class Json() : AsyncReaderTests(CreateJsonSerializer());
+
+	public class MsgPack() : AsyncReaderTests(CreateMsgPackSerializer());
 }
--- test/ShapeShift.Tests/ConverterAttributeTests.cs
+++ test/ShapeShift.Tests/ConverterAttributeTests.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Andrew Arnott. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-public partial class MessagePackConverterAttributeTests : MessagePackSerializerTestBase
+public partial class ConverterAttributeTests : MessagePackSerializerTestBase
 {
 	[Fact]
 	public void CustomTypeWithConverter()
