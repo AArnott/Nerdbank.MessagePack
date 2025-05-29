162dfe3def9c5988e204fe2901aaf30ce292c866
Andrew Arnott <andrewarnott@live.com>
2025-05-28 18:25:18 -0700

    Add TUnit test project

---
+++ test/Nerdbank.MessagePack.TUnit/Usings.cs
@@ -0,0 +1,6 @@
+// Copyright (c) Andrew Arnott. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+global using System.IO.Pipelines;
+global using Nerdbank.MessagePack;
+global using PolyType;
