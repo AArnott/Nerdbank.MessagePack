3076d61acb4241fcff7119692d0d44c53426ddc6
Andrew Arnott <andrewarnott@live.com>
2025-08-01 14:46:01 -0600

    Implement SignalR-messagepack compatible protocol properly

--- test/Nerdbank.MessagePack.SignalR.Tests/MessagePackHubProtocolTests.cs
+++ test/Nerdbank.MessagePack.SignalR.Tests/MessagePackHubProtocolTests.cs
@@ -1,123 +1,126 @@
 // Copyright (c) Andrew Arnott. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Buffers;
 using Microsoft.AspNetCore.Connections;
 using Microsoft.AspNetCore.SignalR.Protocol;
-using Microsoft.Extensions.DependencyInjection;
+using Nerdbank.MessagePack;
+using Nerdbank.MessagePack.SignalR;
+using Nerdbank.Streams;
+using PolyType;
 using Xunit;
 
-namespace Nerdbank.MessagePack.SignalR.Tests;
-
-public class MessagePackHubProtocolTests
+public partial class MessagePackHubProtocolTests
 {
-    private readonly MessagePackHubProtocol protocol;
+	private readonly MessagePackHubProtocol protocol;
 
-    public MessagePackHubProtocolTests()
-    {
-        this.protocol = new MessagePackHubProtocol();
-    }
+	public MessagePackHubProtocolTests()
+	{
+		this.protocol = new MessagePackHubProtocol(Witness.ShapeProvider);
+	}
 
-    [Fact]
-    public void Constructor_CreatesInstance()
-    {
-        var protocol = new MessagePackHubProtocol();
-        
-        Assert.NotNull(protocol);
-        Assert.Equal("messagepack", protocol.Name);
-        Assert.Equal(1, protocol.Version);
-        Assert.Equal(TransferFormat.Binary, protocol.TransferFormat);
-    }
+	[Fact]
+	public void Constructor_CreatesInstance()
+	{
+		var protocol = new MessagePackHubProtocol(Witness.ShapeProvider);
 
-    [Fact]
-    public void Constructor_WithSerializer_CreatesInstance()
-    {
-        var serializer = new MessagePackSerializer();
-        var protocol = new MessagePackHubProtocol(serializer);
-        
-        Assert.NotNull(protocol);
-        Assert.Equal("messagepack", protocol.Name);
-    }
+		Assert.NotNull(protocol);
+		Assert.Equal("messagepack", protocol.Name);
+		Assert.Equal(2, protocol.Version);
+		Assert.Equal(TransferFormat.Binary, protocol.TransferFormat);
+	}
 
-    [Theory]
-    [InlineData(1, true)]
-    [InlineData(0, false)]
-    [InlineData(2, false)]
-    public void IsVersionSupported_ReturnsCorrectResult(int version, bool expected)
-    {
-        var protocol = new MessagePackHubProtocol();
-        
-        Assert.Equal(expected, protocol.IsVersionSupported(version));
-    }
+	[Fact]
+	public void Constructor_WithSerializer_CreatesInstance()
+	{
+		var serializer = new MessagePackSerializer();
+		var protocol = new MessagePackHubProtocol(Witness.ShapeProvider, serializer);
 
-    [Fact]
-    public void GetMessageBytes_PingMessage_ReturnsValidBytes()
-    {
-        var message = PingMessage.Instance;
-        
-        var bytes = this.protocol.GetMessageBytes(message);
-        
-        Assert.True(bytes.Length > 0);
-    }
+		Assert.NotNull(protocol);
+		Assert.Equal("messagepack", protocol.Name);
+	}
 
-    [Fact]
-    public void WriteMessage_PingMessage_WritesToOutput()
-    {
-        var message = PingMessage.Instance;
-        var buffer = new ArrayBufferWriter<byte>();
-        
-        this.protocol.WriteMessage(message, buffer);
-        
-        Assert.NotEqual(0, buffer.WrittenCount);
-    }
+	[Theory]
+	[InlineData(1, true)]
+	[InlineData(2, true)]
+	[InlineData(3, false)]
+	public void IsVersionSupported_ReturnsCorrectResult(int version, bool expected)
+	{
+		var protocol = new MessagePackHubProtocol(Witness.ShapeProvider);
 
-    [Fact]
-    public void GetMessageBytes_CloseMessage_ReturnsValidBytes()
-    {
-        var message = new CloseMessage("Test error", false);
-        
-        var bytes = this.protocol.GetMessageBytes(message);
-        
-        Assert.True(bytes.Length > 0);
-    }
+		Assert.Equal(expected, protocol.IsVersionSupported(version));
+	}
 
-    [Fact]
-    public void GetMessageBytes_InvocationMessage_ReturnsValidBytes()
-    {
-        var message = new InvocationMessage("123", "TestMethod", new object[] { "arg1", 42 });
-        
-        var bytes = this.protocol.GetMessageBytes(message);
-        
-        Assert.True(bytes.Length > 0);
-    }
+	[Fact]
+	public void GetMessageBytes_PingMessage_ReturnsValidBytes()
+	{
+		PingMessage message = PingMessage.Instance;
 
-    [Fact]
-    public void GetMessageBytes_CompletionMessage_ReturnsValidBytes()
-    {
-        var message = CompletionMessage.WithResult("123", "test result");
-        
-        var bytes = this.protocol.GetMessageBytes(message);
-        
-        Assert.True(bytes.Length > 0);
-    }
+		ReadOnlyMemory<byte> bytes = this.protocol.GetMessageBytes(message);
 
-    [Fact]
-    public void GetMessageBytes_StreamItemMessage_ReturnsValidBytes()
-    {
-        var message = new StreamItemMessage("123", "test item");
-        
-        var bytes = this.protocol.GetMessageBytes(message);
-        
-        Assert.True(bytes.Length > 0);
-    }
+		Assert.True(bytes.Length > 0);
+	}
 
-    [Fact]
-    public void GetMessageBytes_CancelInvocationMessage_ReturnsValidBytes()
-    {
-        var message = new CancelInvocationMessage("123");
-        
-        var bytes = this.protocol.GetMessageBytes(message);
-        
-        Assert.True(bytes.Length > 0);
-    }
+	[Fact]
+	public void WriteMessage_PingMessage_WritesToOutput()
+	{
+		PingMessage message = PingMessage.Instance;
+		var buffer = new Sequence<byte>();
+
+		this.protocol.WriteMessage(message, buffer);
+
+		Assert.NotEqual(0, buffer.Length);
+	}
+
+	[Fact]
+	public void GetMessageBytes_CloseMessage_ReturnsValidBytes()
+	{
+		var message = new CloseMessage("Test error", false);
+
+		ReadOnlyMemory<byte> bytes = this.protocol.GetMessageBytes(message);
+
+		Assert.True(bytes.Length > 0);
+	}
+
+	[Fact]
+	public void GetMessageBytes_InvocationMessage_ReturnsValidBytes()
+	{
+		var message = new InvocationMessage("123", "TestMethod", new object[] { "arg1", 42 });
+
+		ReadOnlyMemory<byte> bytes = this.protocol.GetMessageBytes(message);
+
+		Assert.True(bytes.Length > 0);
+	}
+
+	[Fact]
+	public void GetMessageBytes_CompletionMessage_ReturnsValidBytes()
+	{
+		var message = CompletionMessage.WithResult("123", "test result");
+
+		ReadOnlyMemory<byte> bytes = this.protocol.GetMessageBytes(message);
+
+		Assert.True(bytes.Length > 0);
+	}
+
+	[Fact]
+	public void GetMessageBytes_StreamItemMessage_ReturnsValidBytes()
+	{
+		var message = new StreamItemMessage("123", "test item");
+
+		ReadOnlyMemory<byte> bytes = this.protocol.GetMessageBytes(message);
+
+		Assert.True(bytes.Length > 0);
+	}
+
+	[Fact]
+	public void GetMessageBytes_CancelInvocationMessage_ReturnsValidBytes()
+	{
+		var message = new CancelInvocationMessage("123");
+
+		ReadOnlyMemory<byte> bytes = this.protocol.GetMessageBytes(message);
+
+		Assert.True(bytes.Length > 0);
+	}
+
+	[GenerateShapeFor<bool>]
+	private partial class Witness;
 }
