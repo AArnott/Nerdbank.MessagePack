9e007a4d82a441983f5ce70ac197460b9b1163b8
Andrew Arnott <andrew.arnott@microsoft.com>
2025-09-16 09:58:11 -0600

    Offer more property name transformations

--- src/Nerdbank.MessagePack/MessagePackNamingPolicy.cs
+++ src/Nerdbank.MessagePack/MessagePackNamingPolicy.cs
@@ -1,7 +1,10 @@
 ﻿// Copyright (c) Andrew Arnott. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System.Text;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using Microsoft;
 
 namespace Nerdbank.MessagePack;
 
@@ -11,14 +14,40 @@ namespace Nerdbank.MessagePack;
 public abstract class MessagePackNamingPolicy
 {
 	/// <summary>
-	/// Gets a naming policy that converts a .NET PascalCase property to camelCase for msgpack.
+	/// Gets a naming policy that converts identifiers to camelCase
+	/// for serialization purposes.
 	/// </summary>
 	public static MessagePackNamingPolicy CamelCase { get; } = new CamelCaseNamingPolicy();
 
 	/// <summary>
-	/// Gets a naming policy that converts a .NET camelCase property to PascalCase for msgpack.
+	/// Gets a naming policy that converts identifiers to PascalCase
+	/// for serialization purposes.
 	/// </summary>
 	public static MessagePackNamingPolicy PascalCase { get; } = new PascalCaseNamingPolicy();
+
+	/// <summary>
+	/// Gets a naming policy that converts identifiers to lower-case kebab-case (e.g., "example-name")
+	/// for serialization purposes.
+	/// </summary>
+	public static MessagePackNamingPolicy KebabLowerCase { get; } = new KebabLowerCaseNamingPolicy();
+
+	/// <summary>
+	/// Gets a naming policy that converts identifiers to upper-case kebab-case (e.g., "EXAMPLE-NAME")
+	/// for serialization purposes.
+	/// </summary>
+	public static MessagePackNamingPolicy KebabUpperCase { get; } = new KebabUpperCaseNamingPolicy();
+
+	/// <summary>
+	/// Gets a naming policy that converts property names to snake_case (with all lowercase letters)
+	/// for serialization purposes.
+	/// </summary>
+	public static MessagePackNamingPolicy SnakeLowerCase { get; } = new SnakeLowerCaseNamingPolicy();
+
+	/// <summary>
+	/// Gets a naming policy that converts property names to SNAKE_CASE (with all uppercase letters)
+	/// for serialization purposes.
+	/// </summary>
+	public static MessagePackNamingPolicy SnakeUpperCase { get; } = new SnakeUpperCaseNamingPolicy();
 
 	/// <summary>
 	/// Transforms a property name as defined in .NET to a property name as it should be serialized to MessagePack.
@@ -27,135 +56,228 @@ public abstract class MessagePackNamingPolicy
 	/// <returns>The msgpack property name.</returns>
 	public abstract string ConvertName(string name);
 
-	private static string ConvertName(string name, bool toCamelCase)
+	/// <summary>
+	/// A useful base class for identifier transformations based on tokenized words.
+	/// </summary>
+	/// <remarks>
+	/// This class case largely from <see href="https://github.com/dotnet/runtime/blob/b2974279efd059efaa17f359ed4b266b1c705721/src/libraries/System.Text.Json/Common/JsonSeparatorNamingPolicy.cs#L11">this System.Text.Json code</see>.
+	/// </remarks>
+	private abstract class BuiltInPolicy : MessagePackNamingPolicy
 	{
-		if (string.IsNullOrEmpty(name))
+		/// <summary>
+		/// The maximum number of bytes that should be allocated on the stack.
+		/// </summary>
+		private const int StackallocByteThreshold = 256;
+
+		/// <summary>
+		/// The maximum number of characters that should be allocated on the stack.
+		/// </summary>
+		private const int StackallocCharThreshold = StackallocByteThreshold / 2;
+
+		private readonly bool lowercase;
+		private readonly bool capitalizeFirstLetterOfSubsequentWords;
+		private readonly bool capitalizeFirstLetterOfFirstWord;
+		private readonly char? separator;
+
+		internal BuiltInPolicy(bool lowercase, bool capitalizeFirstLetterOfSubsequentWords = false, bool capitalizeFirstLetterOfFirstWord = false, char? separator = null)
 		{
-			return name;
+			Debug.Assert(separator is null || char.IsPunctuation(separator.Value), "Separator is expected to be punctuation.");
+
+			this.lowercase = lowercase;
+			this.separator = separator;
+			this.capitalizeFirstLetterOfFirstWord = capitalizeFirstLetterOfFirstWord;
+			this.capitalizeFirstLetterOfSubsequentWords = capitalizeFirstLetterOfSubsequentWords;
 		}
 
-#if NET
-		return string.Create(name.Length, (name, toCamelCase), static (span, state) =>
+		private enum SeparatorState
 		{
-			bool firstWord = true;
-			int i = 0;
-			int outputPosition = 0;
-			while (i < state.name.Length)
+			NotStarted,
+			UppercaseLetter,
+			LowercaseLetterOrDigit,
+			SpaceSeparator,
+		}
+
+		public sealed override string ConvertName(string name)
+		{
+			Requires.NotNull(name);
+
+			return ConvertNameCore(this.separator, this.lowercase, this.capitalizeFirstLetterOfSubsequentWords, this.capitalizeFirstLetterOfFirstWord, name.AsSpan());
+		}
+
+		private static string ConvertNameCore(char? separator, bool lowercase, bool capitalizeFirstLetterOfSubsequentWords, bool capitalizeFirstLetterOfFirstWord, ReadOnlySpan<char> chars)
+		{
+			char[]? rentedBuffer = null;
+
+			// While we can't predict the expansion factor of the resultant string,
+			// start with a buffer that is at least 20% larger than the input.
+			int initialBufferLength = (int)(1.2 * chars.Length);
+			Span<char> destination = initialBufferLength <= StackallocCharThreshold
+				? stackalloc char[StackallocCharThreshold]
+				: (rentedBuffer = ArrayPool<char>.Shared.Rent(initialBufferLength));
+
+			SeparatorState state = SeparatorState.NotStarted;
+			int charsWritten = 0;
+			bool nextCharacterStartsFirstWord = true;
+			bool nextCharacterStartsSubsequentWord = false;
+
+			for (int i = 0; i < chars.Length; i++)
 			{
-				if (!char.IsLetterOrDigit(state.name[i]))
-				{
-					span[outputPosition++] = state.name[i++];
-					firstWord = true;
-					continue;
-				}
+				// NB this implementation does not handle surrogate pair letters
+				// cf. https://github.com/dotnet/runtime/issues/90352
+				char current = chars[i];
+				UnicodeCategory category = char.GetUnicodeCategory(current);
 
-				int wordLength = 1;
-				while (i + wordLength < state.name.Length && char.IsLower(state.name[i + wordLength]))
+				switch (category)
 				{
-					wordLength++;
-				}
+					case UnicodeCategory.UppercaseLetter:
 
-				if (wordLength == 1)
-				{
-					while (i + wordLength < state.name.Length && char.IsUpper(state.name[i + wordLength]) && (i + wordLength + 1 >= state.name.Length || !char.IsLower(state.name[i + wordLength + 1])))
-					{
-						wordLength++;
-					}
-				}
-
-				ReadOnlySpan<char> word = state.name.AsSpan(i, wordLength);
-				if (firstWord)
-				{
-					if (state.toCamelCase)
-					{
-						for (int j = 0; j < word.Length; j++)
+						switch (state)
 						{
-							span[outputPosition++] = char.ToLowerInvariant(word[j]);
+							case SeparatorState.NotStarted:
+								break;
+
+							case SeparatorState.LowercaseLetterOrDigit:
+							case SeparatorState.SpaceSeparator:
+								// An uppercase letter following a sequence of lowercase letters or spaces
+								// denotes the start of a new grouping: emit a separator character.
+								if (separator.HasValue)
+								{
+									WriteChar(separator.Value, ref destination);
+								}
+
+								nextCharacterStartsSubsequentWord = true;
+								break;
+
+							case SeparatorState.UppercaseLetter:
+								// We are reading through a sequence of two or more uppercase letters.
+								// Uppercase letters are grouped together with the exception of the
+								// final letter, assuming it is followed by lowercase letters.
+								// For example, the value 'XMLReader' should render as 'xml_reader',
+								// however 'SHA512Hash' should render as 'sha512-hash'.
+								if (i + 1 < chars.Length && char.IsLower(chars[i + 1]))
+								{
+									if (separator.HasValue)
+									{
+										WriteChar(separator.Value, ref destination);
+									}
+
+									nextCharacterStartsSubsequentWord = true;
+								}
+
+								break;
+
+							default:
+								Debug.Fail($"Unexpected state {state}");
+								break;
 						}
-					}
-					else
-					{
-						span[outputPosition++] = char.ToUpperInvariant(word[0]);
-						word.Slice(1).CopyTo(span.Slice(outputPosition));
-						outputPosition += word.Length - 1;
-					}
 
-					firstWord = false;
+						if (lowercase && !((nextCharacterStartsSubsequentWord && capitalizeFirstLetterOfSubsequentWords) || (nextCharacterStartsFirstWord && capitalizeFirstLetterOfFirstWord)))
+						{
+							current = char.ToLowerInvariant(current);
+						}
+
+						WriteChar(current, ref destination);
+						nextCharacterStartsFirstWord = false;
+						nextCharacterStartsSubsequentWord = false;
+						state = SeparatorState.UppercaseLetter;
+						break;
+
+					case UnicodeCategory.LowercaseLetter:
+					case UnicodeCategory.DecimalDigitNumber:
+
+						if (state is SeparatorState.SpaceSeparator)
+						{
+							if (separator.HasValue)
+							{
+								// Normalize preceding spaces to one separator.
+								WriteChar(separator.Value, ref destination);
+							}
+
+							nextCharacterStartsSubsequentWord = true;
+						}
+
+						bool shouldBeUpperCase = !lowercase ||
+							((nextCharacterStartsSubsequentWord && capitalizeFirstLetterOfSubsequentWords) ||
+							(nextCharacterStartsFirstWord && capitalizeFirstLetterOfFirstWord));
+						if (shouldBeUpperCase && category is UnicodeCategory.LowercaseLetter)
+						{
+							current = char.ToUpperInvariant(current);
+						}
+
+						WriteChar(current, ref destination);
+						nextCharacterStartsFirstWord = false;
+						nextCharacterStartsSubsequentWord = false;
+						state = SeparatorState.LowercaseLetterOrDigit;
+						break;
+
+					case UnicodeCategory.SpaceSeparator:
+						// Space characters are trimmed from the start and end of the input string
+						// but are normalized to separator characters if between letters.
+						if (state != SeparatorState.NotStarted)
+						{
+							state = SeparatorState.SpaceSeparator;
+						}
+
+						break;
+
+					default:
+						// Non-alphanumeric characters (including the separator character and surrogates)
+						// are written as-is to the output and reset the separator state.
+						// E.g. 'ABC???def' maps to 'abc???def' in snake_case.
+						WriteChar(current, ref destination);
+						state = SeparatorState.NotStarted;
+						break;
 				}
-				else
+			}
+
+			string result = destination.Slice(0, charsWritten).ToString();
+
+			if (rentedBuffer is not null)
+			{
+				destination.Slice(0, charsWritten).Clear();
+				ArrayPool<char>.Shared.Return(rentedBuffer);
+			}
+
+			return result;
+
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			void WriteChar(char value, ref Span<char> destination)
+			{
+				if (charsWritten == destination.Length)
 				{
-					span[outputPosition++] = char.ToUpperInvariant(word[0]);
-					word.Slice(1).CopyTo(span.Slice(outputPosition));
-					outputPosition += word.Length - 1;
+					ExpandBuffer(ref destination);
 				}
 
-				i += wordLength;
+				destination[charsWritten++] = value;
 			}
 
-			span.Slice(outputPosition).Clear();
-		});
-#else
-		StringBuilder sb = new StringBuilder(name.Length);
-		bool firstWord = true;
-		int i = 0;
-		while (i < name.Length)
-		{
-			if (!char.IsLetterOrDigit(name[i]))
+			void ExpandBuffer(ref Span<char> destination)
 			{
-				sb.Append(name[i++]);
-				firstWord = true;
-				continue;
-			}
+				int newSize = checked(destination.Length * 2);
+				char[] newBuffer = ArrayPool<char>.Shared.Rent(newSize);
+				destination.CopyTo(newBuffer);
 
-			int wordLength = 1;
-			while (i + wordLength < name.Length && char.IsLower(name[i + wordLength]))
-			{
-				wordLength++;
-			}
-
-			if (wordLength == 1)
-			{
-				while (i + wordLength < name.Length && char.IsUpper(name[i + wordLength]) && (i + wordLength + 1 >= name.Length || !char.IsLower(name[i + wordLength + 1])))
+				if (rentedBuffer is not null)
 				{
-					wordLength++;
-				}
-			}
-
-			string word = name.Substring(i, wordLength);
-			if (firstWord)
-			{
-				if (toCamelCase)
-				{
-					sb.Append(word.ToLowerInvariant());
-				}
-				else
-				{
-					sb.Append(char.ToUpperInvariant(word[0]));
-					sb.Append(word.Substring(1));
+					destination.Slice(0, charsWritten).Clear();
+					ArrayPool<char>.Shared.Return(rentedBuffer);
 				}
 
-				firstWord = false;
+				rentedBuffer = newBuffer;
+				destination = rentedBuffer;
 			}
-			else
-			{
-				sb.Append(char.ToUpperInvariant(word[0]));
-				sb.Append(word.Substring(1));
-			}
-
-			i += wordLength;
 		}
-
-		return sb.ToString();
-#endif
 	}
 
-	private class CamelCaseNamingPolicy : MessagePackNamingPolicy
-	{
-		public override string ConvertName(string name) => ConvertName(name, toCamelCase: true);
-	}
+	private class CamelCaseNamingPolicy() : BuiltInPolicy(lowercase: true, capitalizeFirstLetterOfSubsequentWords: true);
 
-	private class PascalCaseNamingPolicy : MessagePackNamingPolicy
-	{
-		public override string ConvertName(string name) => ConvertName(name, toCamelCase: false);
-	}
+	private class PascalCaseNamingPolicy() : BuiltInPolicy(lowercase: true, capitalizeFirstLetterOfFirstWord: true, capitalizeFirstLetterOfSubsequentWords: true);
+
+	private class KebabLowerCaseNamingPolicy() : BuiltInPolicy(lowercase: true, separator: '-');
+
+	private class KebabUpperCaseNamingPolicy() : BuiltInPolicy(lowercase: false, separator: '-');
+
+	private class SnakeLowerCaseNamingPolicy() : BuiltInPolicy(lowercase: true, separator: '_');
+
+	private class SnakeUpperCaseNamingPolicy() : BuiltInPolicy(lowercase: false, separator: '_');
 }
