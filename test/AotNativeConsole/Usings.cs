5d84554c30c1ebe9bd84bf830cb2a7f56e0b7738
Andrew Arnott <andrewarnott@live.com>
2025-11-02 21:58:25 -0700

    Add switch for testing cold startup perf

--- test/AotNativeConsole/Usings.cs
+++ test/AotNativeConsole/Usings.cs
@@ -1,5 +1,8 @@
 // Copyright (c) Andrew Arnott. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+global using System.Diagnostics;
 global using System.Drawing;
 global using System.Numerics;
+global using Nerdbank.MessagePack;
+global using PolyType;
