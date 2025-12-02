c44196caac4e5fe94bd35711b0acec58abeb9fee
Andrew Arnott <andrew.arnott@microsoft.com>
2025-12-01 21:32:28 -0700

    Fix xml doc comments to refer to actually existing overloads

--- src/Nerdbank.MessagePack/DerivedShapeMapping`1.cs
+++ src/Nerdbank.MessagePack/DerivedShapeMapping`1.cs
@@ -51,7 +51,7 @@ public class DerivedShapeMapping<TBase> : DerivedTypeUnion, IDerivedTypeMapping,
 	/// <param name="alias">The alias for the sub-type.</param>
 	/// <param name="provider"><inheritdoc cref="MessagePackSerializer.CreateSerializationContext(ITypeShapeProvider, CancellationToken)" path="/param[@name='provider']"/></param>
 	/// <exception cref="ArgumentException">Thrown when <paramref name="alias"/> or <typeparamref name="TDerived"/> has already been added to this mapping.</exception>
-	[System.Diagnostics.CodeAnalysis.SuppressMessage("ApiDesign", "RS0030:Do not use banned APIs", Justification = "Public API accepts an ITypeShapeProvider.")]
+	[SuppressMessage("ApiDesign", "RS0030:Do not use banned APIs", Justification = "Public API accepts an ITypeShapeProvider.")]
 	public void Add<TDerived>(DerivedTypeIdentifier alias, ITypeShapeProvider provider)
 		where TDerived : TBase
 	{
