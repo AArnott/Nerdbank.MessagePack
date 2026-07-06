bcf63537276c79416a41786b6f8b6c9875e0b92b
Andrew Arnott <andrew.arnott@microsoft.com>
2026-07-06 13:10:14 -0600

    Consolidate test helpers and add more empty sequence segment tests

--- test/Nerdbank.MessagePack.Tests/MessagePackReaderTests.ReadString.cs
+++ test/Nerdbank.MessagePack.Tests/MessagePackReaderTests.ReadString.cs
@@ -10,7 +10,7 @@ public partial class MessagePackReaderTests
 	[Fact]
 	public void ReadString_HandlesSingleSegment()
 	{
-		ReadOnlySequence<byte> seq = this.BuildSequence(new[]
+		ReadOnlySequence<byte> seq = SequenceBuilder.Create(new[]
 		{
 			(byte)(MessagePackCode.MinFixStr + 2),
 			(byte)'A', (byte)'B',
@@ -24,8 +24,21 @@ public partial class MessagePackReaderTests
 	[Fact]
 	public void ReadString_HandlesMultipleSegments()
 	{
-		ReadOnlySequence<byte> seq = this.BuildSequence(
+		ReadOnlySequence<byte> seq = SequenceBuilder.Create(
 			new[] { (byte)(MessagePackCode.MinFixStr + 2), (byte)'A' },
+			new[] { (byte)'B' });
+
+		var reader = new MessagePackReader(seq);
+		var result = reader.ReadString();
+		Assert.Equal("AB", result);
+	}
+
+	[Fact]
+	public void ReadString_HandlesMultipleSegments_WithEmptySegment()
+	{
+		ReadOnlySequence<byte> seq = SequenceBuilder.Create(
+			new[] { (byte)(MessagePackCode.MinFixStr + 2), (byte)'A' },
+			[],
 			new[] { (byte)'B' });
 
 		var reader = new MessagePackReader(seq);
@@ -37,7 +50,7 @@ public partial class MessagePackReaderTests
 	[Trait("CWE", "682")]
 	public void ReadString_HandlesMultipleSegments_WithExpectedRemainingStructures()
 	{
-		ReadOnlySequence<byte> seq = this.BuildSequence(
+		ReadOnlySequence<byte> seq = SequenceBuilder.Create(
 			new[] { (byte)(MessagePackCode.MinFixArray + 2), (byte)(MessagePackCode.MinFixStr + 3), (byte)'A' },
 			new[] { (byte)'B', (byte)'C', (byte)MessagePackCode.Nil });
 
@@ -66,38 +79,4 @@ public partial class MessagePackReaderTests
 	private static extern uint GetExpectedRemainingStructures(ref MessagePackReader reader);
 #endif
 
-	private ReadOnlySequence<T> BuildSequence<T>(params T[][] segmentContents)
-	{
-		if (segmentContents.Length == 1)
-		{
-			return new ReadOnlySequence<T>(segmentContents[0].AsMemory());
-		}
-
-		var bufferSegment = new BufferSegment<T>(segmentContents[0].AsMemory());
-		BufferSegment<T>? last = default;
-		for (var i = 1; i < segmentContents.Length; i++)
-		{
-			last = bufferSegment.Append(segmentContents[i]);
-		}
-
-		return new ReadOnlySequence<T>(bufferSegment, 0, last!, last!.Memory.Length);
-	}
-
-	internal class BufferSegment<T> : ReadOnlySequenceSegment<T>
-	{
-		public BufferSegment(ReadOnlyMemory<T> memory)
-		{
-			this.Memory = memory;
-		}
-
-		public BufferSegment<T> Append(ReadOnlyMemory<T> memory)
-		{
-			var segment = new BufferSegment<T>(memory)
-			{
-				RunningIndex = this.RunningIndex + this.Memory.Length,
-			};
-			this.Next = segment;
-			return segment;
-		}
-	}
 }
